19518902d67bb150c0262fc28d5bfadb1b02e498
Mario Aldag <mario@aldagnw.de>
2022-06-18 12:14:34 +0200

    Refactoring nach Kapitel 8.1
    * Service definieren und dann die Bookstore Definition dahin auslagern.
    * Im Konstruktor der Komponente dann per (private bs: ...) eine automatische Referenz auf den Service bekommen.

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Book } from '../shared/book'
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'bm-book-list',
@@ -8,41 +9,15 @@ import { Book } from '../shared/book'
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
-  books: Book[];
+  books?: Book[];
   @Output() showDetailsEvent = new EventEmitter<Book>();
 
-  constructor() {
-    this.books = [
-      {
-        isbn: '978386497791',
-        title: 'Angular',
-        authors: ['Ferdinand Malcher', 'Johannes Hoppe', 'Danny Kopenhagen'],
-        published: new Date(2020,8,1),
-        subtitle: 'Grundlagen, fortgeschrittene Themen und Best Practises',
-        rating: 5,
-        thumbnails: [{
-          url: 'https://ng-buch.de/angular-cover.jpg',
-          title: 'Buchcover'
-        }],
-        description: 'Lernen Sie Angular mit diesem Praxisbuch'
-      },
-      {
-        isbn: '9783864905520',
-        title: 'React',
-        authors: ['Oliver Zeigermann', 'Nils Hartmann'],
-        published: new Date(2019,11,12),
-        subtitle: 'Grundlagen, fortgeschrittene Themen und Praxistipps',
-        rating: 3,
-        thumbnails: [{
-          url: 'https://ng-buch.de/react-cover.jpg',
-          title: 'Buchcover'
-        }],
-        description: 'Das bewährte und umfassende Praxisbuch zu React.'
-      }
-    ];
+  constructor(private bs: BookStoreService) {
+
    }
 
   ngOnInit(): void {
+    this.books = this.bs.getAll();
   }
 
   showDetails(book:Book) {
